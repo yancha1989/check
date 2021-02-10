d04e865f49bfd93869c4705df1529c0676f24621
yancha1989 <34509508+yancha1989@users.noreply.github.com>
2021-02-10 19:33:12 +0300

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -2,10 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom";
 import {BrowserRouter} from 'react-router-dom';
 import App from "./components/App"
+import Try from "../TryServer/Try"
 
 
 ReactDOM.render(
     <BrowserRouter>
-        <App />
+        <Try />
     </BrowserRouter>,
 document.getElementById("root"));
+    
